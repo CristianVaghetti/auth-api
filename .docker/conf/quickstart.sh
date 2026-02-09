314ee68b3732f6ed49fc175e3fa2eada5a799c3e
Cristian Vaghetti <vaghetticristian@gmail.com>
2026-02-09 08:51:15 -0300

    fix - ajuste comando instalar as dependencias ao subir container

--- .docker/conf/quickstart.sh
+++ .docker/conf/quickstart.sh
@@ -5,8 +5,8 @@ set -e
 if [ ! -d "/var/www/html/vendor" ]; then
     /bin/echo "<pre>" > /var/www/html/public/install.html
 
-    php /var/www/html/composer.phar self-update 2>> /var/www/html/public/install.html
-    php /var/www/html/composer.phar install -d /var/www/html  2>> /var/www/html/public/install.html 
+    composer self-update 2>> /var/www/html/public/install.html
+    composer install -d /var/www/html  2>> /var/www/html/public/install.html 
 
     /bin/rm -f /var/www/html/public/install.html
 
